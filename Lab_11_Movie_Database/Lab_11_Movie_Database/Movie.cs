e6ecf81c0e9968bae7b77cc8fea2c063ce5d8523
kmarion76 <47949532+kmarion76@users.noreply.github.com>
2021-02-15 18:11:59 -0500

    lab 11

---
+++ Lab_11_Movie_Database/Lab_11_Movie_Database/Movie.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11_Movie_Database
+{
+    public class Movie
+    {
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+
+        public Movie()
+        {
+
+        }
+
+        private string _category;
+        private string _title;
+
+        public void GetHorror()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "horror")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+        public void GetAnimated()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "animated")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+        public void GetSciFi()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "sci-fi")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+        public void GetDrama()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "drama")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+        private List<Movie> InitializeList()
+        {
+            var list = new List<Movie>();
+
+            list.Add(new Movie("Spirited Away", "animated"));
+            list.Add(new Movie("Frozen", "animated"));
+            list.Add(new Movie("Rambo", "drama"));
+            list.Add(new Movie("Django Unchained", "drama"));
+            list.Add(new Movie("Texas Chainsaw Massacre", "horror"));
+            list.Add(new Movie("Saw", "horror"));
+            list.Add(new Movie("Star Wars", "sci-fi"));
+            list.Add(new Movie("Jurassic", "sci-fi"));
+            list.Add(new Movie("Annabelle", "horror"));
+            list.Add(new Movie("The Conjouring", "horror"));
+
+            return list;
+        }
+    }
+}
---
+++ Lab_11_Movie_Database/Lab_11_Movie_Database/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_11_Movie_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMovie;
+            string userContinue;
+            Console.WriteLine("Welcome to the Movie List Application");
+            Console.WriteLine("There are 10 movies in this list");
+            
+
+            do
+            {
+                Console.WriteLine("What category are you interested in?");
+                userMovie = Console.ReadLine();
+                Console.WriteLine(string.Empty);
+
+                if (userMovie.Equals("scifi", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var scifi = new Movie();
+                    scifi.GetSciFi();
+                }
+
+                else if (userMovie.Equals("horror", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var horror = new Movie();
+                    horror.GetHorror();
+                }
+
+                else if (userMovie.Equals("drama", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var drama = new Movie();
+                    drama.GetDrama();
+                }
+
+                else if (userMovie.Equals("animated", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var animated = new Movie();
+                    animated.GetAnimated();
+                }
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("Would you like to continue (y/n)? ");
+                userContinue = Console.ReadLine();
+
+            } while (userContinue == "y");
+            Console.WriteLine("Goodbye!");
+
+            
+            
+            
+
+
+
+
+        }
+    }
+}
